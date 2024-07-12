f8d5220fde8a20ee5c5d41b377bbe0ba1f07e37e
DanielCavalcantih <danielcavalcanti8000@gmail.com>
2024-07-12 17:51:51 -0300

    Ajustes package

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = require('./app');
 const port = process.env.API_PORT || 3001;
 
 app.get('/', (_req, res) => {
-  res.send();
+  res.send("Olá Daniel!");
 });
 
 app.listen(port, () => console.log('ouvindo porta', port));
