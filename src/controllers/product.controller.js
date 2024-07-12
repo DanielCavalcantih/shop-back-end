466441f55ae3b4994980ee2cb6c50ab11d64f4a5
DanielCavalcantih <danielcavalcanti8000@gmail.com>
2024-07-12 00:56:27 -0300

    Ajuste tratamento de erros

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -14,19 +14,19 @@ const getProducts = async (req, res) => {
 const insertProduct = async (req, res) => {
   const { id } = req.payload;
   const newProduct = await productService.insertProduct(req.body, id);
-  return res.status(201).json(newProduct);
+  return res.status(201).json({ message: "Produto adicionado com sucesso!" });
 };
 
 const updateProduct = async (req, res) => {
   const { id } = req.params;
   const updatedProduct = await productService.updateProduct(req.body, id);
-  return res.status(200).json(updatedProduct);
+  return res.status(200).json({message: "Produto editado com sucesso!" });
 };
 
 const deleteProduct = async (req, res) => {
   const { id } = req.params;
   await productService.deleteProduct(id);
-  return res.status(202).json({ message: 'Deleted!' });
+  return res.status(204).json({ message: "Produto deletado com sucesso!" });
 };
 
 module.exports = {
