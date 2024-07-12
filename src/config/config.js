5cd45e67e9caedf37594e717f84b6b38975cf9c7
DanielCavalcantih <danielcavalcanti8000@gmail.com>
2024-07-12 03:25:12 -0300

    Ajustes vercel

--- src/config/config.js
+++ src/config/config.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 const environment = process.env.NODE_ENV || 'development';
 
 const options = {
-  host: process.env.MYSQL_HOST || 'localhost',
+  host: process.env.MYSQL_HOST || 'shop-database.ct664mio6cyp.us-east-1.rds.amazonaws.com',
   port: process.env.MYSQL_PORT || '3306',
-  database: process.env.MYSQL_DB_NAME || 'shop',
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'jato1234',
+  database: process.env.MYSQL_DB_NAME || 'shop-database',
+  username: process.env.MYSQL_USER || 'admin',
+  password: process.env.MYSQL_PASSWORD || 'Jato1234',
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
